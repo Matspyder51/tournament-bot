8d0675768efd7f9b5b08d5717f2320e01dc72e8b
Mathis Cucci <mat.spyder@gmail.com>
2021-05-08 21:18:41 +0200

    Fix a possible bug on game logs

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
 import { words } from './words.json';
 
 export function GetRandomWord(): string {
-	return words[GetRandomNumber(0, words.length - 1, true)];
+	const index = GetRandomNumber(0, words.length - 1, true);
+	return words[index < words.length ? index : index - 1];
 }
 
 export function GetRandomNumber(min: number, max: number, round?: boolean): number {

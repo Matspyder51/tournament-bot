2ee6e96fe7db8db89ea848f212d6f232fa1f468c
Mathis Cucci <mat.spyder@gmail.com>
2021-05-28 04:37:17 +0200

    Create "AddParticipant" func to Team Model

--- src/models/team.ts
+++ src/models/team.ts
@@ -3,6 +3,15 @@ import { Participant } from "./participant";
 export class Team {
 	constructor(public players: Participant[]) {}
 
+	public AddParticipant(participant: Participant): boolean {
+		if (this.players.indexOf(participant) != -1)
+			return false;
+
+		this.players.push(participant);
+
+		return true;
+	}
+
 	public toString(checkIfFree?: boolean, long?: boolean) {
 		let msg = '';
 		let isFirst = true;

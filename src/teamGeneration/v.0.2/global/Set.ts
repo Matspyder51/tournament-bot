0f3f741de72cf1b3145fd2ce7ec99bfb734bc21f
Etienne <etienne.quantin@danaen.fr>
2021-09-09 21:34:00 +0200

    wip

--- src/teamGeneration/v.0.2/global/Set.ts
+++ src/teamGeneration/v.0.2/global/Set.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export {};
 declare global {
     interface Set<T> {
@@ -12,10 +13,8 @@ declare global {
 
 // The find() method returns the value of the first element in the provided set that satisfies the provided testing function.
 // If no values satisfy the testing function, undefined is returned.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Set.prototype.find = function (this: Set<any>, callbackfn: (element: any, index?: number, set?: Set<any>) => boolean): any | undefined {
 	let i = 0;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	let e: any;
 	this.forEach(element => {
 		if (callbackfn(element, i, this) === true) e = element;
@@ -25,22 +24,18 @@ Set.prototype.find = function (this: Set<any>, callbackfn: (element: any, index?
 };
 
 // The first() method returns the first element from the provided set.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Set.prototype.first = function (this: Set<any>): any {
 	return this.entries().next().value[0];
 };
 
 // The rand() method returns a random element from the provided set.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Set.prototype.rand = function (this: Set<any>): any {
 	return Array.from(this)[Math.floor(Math.random() * this.size)];
 };
 
 // filter
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Set.prototype.filter = function (this: Set<any>, callbackfn: (element: any, index?: number, set?: Set<any>) => boolean): Set<any> {
 	let i = 0;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const newSet: Set<any> = new Set();
 	this.forEach(element => {
 		if (callbackfn(element, i, this) === true) newSet.add(element);
@@ -50,10 +45,8 @@ Set.prototype.filter = function (this: Set<any>, callbackfn: (element: any, inde
 };
 
 // map
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Set.prototype.map = function (this: Set<any>, callbackfn: (element: any, index?: number, set?: Set<any>) => any): Set<any> {
 	let i = 0;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const newSet: Set<any> = new Set();
 	this.forEach(element => {
 		const e = callbackfn(element, i, this);
@@ -64,7 +57,6 @@ Set.prototype.map = function (this: Set<any>, callbackfn: (element: any, index?:
 };
 
 // clone
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Set.prototype.clone = function (this: Set<any>): Set<any> {
 	return new Set(this);
 };
